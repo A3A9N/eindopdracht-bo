e173095f556b77750fb8572de03a0bcbf40dcc59
A3A9N <144899095+A3A9N@users.noreply.github.com>
2023-11-03 12:08:37 +0100

    Game

---
+++ Timmy.js
@@ -0,0 +1,97 @@
+console.log("\r\n\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2557\u2588\u2588\u2588\u2557   \u2588\u2588\u2588\u2557\u2588\u2588\u2588\u2557   \u2588\u2588\u2588\u2557\u2588\u2588\u2557   \u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557     \u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2557   \u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2588\u2557   \u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2557   \u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\r\n\u255A\u2550\u2550\u2588\u2588\u2554\u2550\u2550\u255D\u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2551\u255A\u2588\u2588\u2557 \u2588\u2588\u2554\u255D\u2588\u2588\u2554\u2550\u2550\u2550\u2550\u255D    \u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2551   \u2588\u2588\u2551\u2588\u2588\u2554\u2550\u2550\u2550\u2550\u255D\u2588\u2588\u2588\u2588\u2557  \u2588\u2588\u2551\u255A\u2550\u2550\u2588\u2588\u2554\u2550\u2550\u255D\u2588\u2588\u2551   \u2588\u2588\u2551\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2554\u2550\u2550\u2550\u2550\u255D\r\n   \u2588\u2588\u2551   \u2588\u2588\u2551\u2588\u2588\u2554\u2588\u2588\u2588\u2588\u2554\u2588\u2588\u2551\u2588\u2588\u2554\u2588\u2588\u2588\u2588\u2554\u2588\u2588\u2551 \u255A\u2588\u2588\u2588\u2588\u2554\u255D \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557    \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2551\u2588\u2588\u2551  \u2588\u2588\u2551\u2588\u2588\u2551   \u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2588\u2557  \u2588\u2588\u2554\u2588\u2588\u2557 \u2588\u2588\u2551   \u2588\u2588\u2551   \u2588\u2588\u2551   \u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255D\u2588\u2588\u2588\u2588\u2588\u2557  \r\n   \u2588\u2588\u2551   \u2588\u2588\u2551\u2588\u2588\u2551\u255A\u2588\u2588\u2554\u255D\u2588\u2588\u2551\u2588\u2588\u2551\u255A\u2588\u2588\u2554\u255D\u2588\u2588\u2551  \u255A\u2588\u2588\u2554\u255D  \u255A\u2550\u2550\u2550\u2550\u2588\u2588\u2551    \u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2551\u2588\u2588\u2551  \u2588\u2588\u2551\u255A\u2588\u2588\u2557 \u2588\u2588\u2554\u255D\u2588\u2588\u2554\u2550\u2550\u255D  \u2588\u2588\u2551\u255A\u2588\u2588\u2557\u2588\u2588\u2551   \u2588\u2588\u2551   \u2588\u2588\u2551   \u2588\u2588\u2551\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2554\u2550\u2550\u255D  \r\n   \u2588\u2588\u2551   \u2588\u2588\u2551\u2588\u2588\u2551 \u255A\u2550\u255D \u2588\u2588\u2551\u2588\u2588\u2551 \u255A\u2550\u255D \u2588\u2588\u2551   \u2588\u2588\u2551   \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2551    \u2588\u2588\u2551  \u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255D \u255A\u2588\u2588\u2588\u2588\u2554\u255D \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2551 \u255A\u2588\u2588\u2588\u2588\u2551   \u2588\u2588\u2551   \u255A\u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255D\u2588\u2588\u2551  \u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\r\n   \u255A\u2550\u255D   \u255A\u2550\u255D\u255A\u2550\u255D     \u255A\u2550\u255D\u255A\u2550\u255D     \u255A\u2550\u255D   \u255A\u2550\u255D   \u255A\u2550\u2550\u2550\u2550\u2550\u2550\u255D    \u255A\u2550\u255D  \u255A\u2550\u255D\u255A\u2550\u2550\u2550\u2550\u2550\u255D   \u255A\u2550\u2550\u2550\u255D  \u255A\u2550\u2550\u2550\u2550\u2550\u2550\u255D\u255A\u2550\u255D  \u255A\u2550\u2550\u2550\u255D   \u255A\u2550\u255D    \u255A\u2550\u2550\u2550\u2550\u2550\u255D \u255A\u2550\u255D  \u255A\u2550\u255D\u255A\u2550\u2550\u2550\u2550\u2550\u2550\u255D\r\n                                                                                                                                    \r\n")
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let playerName = '';
+
+function startGame() {
+  console.log('Welkom bij Timmy\'s avontuur!');
+  rl.question('Voer je naam in: ', (name) => {
+    playerName = name;
+    console.log(`Welkom, ${playerName}! Timmy gaat op avontuur.`);
+    choosePath();
+  });
+}
+
+function choosePath() {
+  console.log(`Je staat voor een splitsing in het pad, ${playerName}.`);
+  rl.question('A. Neem het linkerpad of B. Neem het rechterpad: ', (choice) => {
+    if (choice.toLowerCase() === 'a') {
+      crossBridge();
+    } else if (choice.toLowerCase() === 'b') {
+      swimRiver();
+    } else {
+      console.log('Ongeldige keuze. Probeer opnieuw.');
+      choosePath();
+    }
+  });
+}
+
+function crossBridge() {
+  console.log('Je komt een brug tegen.');
+  rl.question('A. Steek de brug over of B. Zwem door de rivier: ', (choice) => {
+    if (choice.toLowerCase() === 'a') {
+      meetWizard();
+    } else if (choice.toLowerCase() === 'b') {
+      console.log('Je verdrinkt in de rivier. Einde 2: Game over.');
+      rl.close();
+    } else {
+      console.log('Ongeldige keuze. Probeer opnieuw.');
+      crossBridge();
+    }
+  });
+}
+
+function swimRiver() {
+  console.log('Je zwemt door de rivier en komt een tovenaar tegen.');
+  rl.question('A. Vraag om hulp of B. Ga verder zonder hulp: ', (choice) => {
+    if (choice.toLowerCase() === 'a') {
+      askWizardForHelp();
+    } else if (choice.toLowerCase() === 'b') {
+      goBack();
+    } else {
+      console.log('Ongeldige keuze. Probeer opnieuw.');
+      swimRiver();
+    }
+  });
+}
+
+function meetWizard() {
+  console.log('Je komt een tovenaar tegen.');
+  rl.question('A. Vraag om hulp of B. Ga verder zonder hulp: ', (choice) => {
+    if (choice.toLowerCase() === 'a') {
+      askWizardForHelp();
+    } else if (choice.toLowerCase() === 'b') {
+      goBack();
+    } else {
+      console.log('Ongeldige keuze. Probeer opnieuw.');
+      meetWizard();
+    }
+  });
+}
+
+function askWizardForHelp() {
+  console.log('De tovenaar biedt je hulp aan, maar hij wil een sleutel in ruil.');
+  rl.question('A. Vraag de tovenaar om de sleutel of B. Ga terug: ', (choice) => {
+    if (choice.toLowerCase() === 'a') {
+      console.log(`Einde 3: Je hebt de sleutel en gaat verder, ${playerName}!`);
+      rl.close();
+    } else if (choice.toLowerCase() === 'b') {
+      goBack();
+    } else {
+      console.log('Ongeldige keuze. Probeer opnieuw.');
+      askWizardForHelp();
+    }
+  });
+}
+
+function goBack() {
+  console.log(`Je gaat terug, ${playerName}. Einde 4: Je avontuur eindigt.`);
+  rl.close();
+}
+
+startGame();
