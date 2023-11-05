3b1e877bc9e2a121d423c5124388c022b8c190cb
A3A9N <144899095+A3A9N@users.noreply.github.com>
2023-11-05 21:31:27 +0100

    Eind

--- Timmy.js
+++ Timmy.js
@@ -1,97 +1,190 @@
 console.log("\r\n\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2557\u2588\u2588\u2588\u2557   \u2588\u2588\u2588\u2557\u2588\u2588\u2588\u2557   \u2588\u2588\u2588\u2557\u2588\u2588\u2557   \u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557     \u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2557   \u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2588\u2557   \u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2557   \u2588\u2588\u2557\u2588\u2588\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\r\n\u255A\u2550\u2550\u2588\u2588\u2554\u2550\u2550\u255D\u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2557 \u2588\u2588\u2588\u2588\u2551\u255A\u2588\u2588\u2557 \u2588\u2588\u2554\u255D\u2588\u2588\u2554\u2550\u2550\u2550\u2550\u255D    \u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2551   \u2588\u2588\u2551\u2588\u2588\u2554\u2550\u2550\u2550\u2550\u255D\u2588\u2588\u2588\u2588\u2557  \u2588\u2588\u2551\u255A\u2550\u2550\u2588\u2588\u2554\u2550\u2550\u255D\u2588\u2588\u2551   \u2588\u2588\u2551\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2554\u2550\u2550\u2550\u2550\u255D\r\n   \u2588\u2588\u2551   \u2588\u2588\u2551\u2588\u2588\u2554\u2588\u2588\u2588\u2588\u2554\u2588\u2588\u2551\u2588\u2588\u2554\u2588\u2588\u2588\u2588\u2554\u2588\u2588\u2551 \u255A\u2588\u2588\u2588\u2588\u2554\u255D \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557    \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2551\u2588\u2588\u2551  \u2588\u2588\u2551\u2588\u2588\u2551   \u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2588\u2557  \u2588\u2588\u2554\u2588\u2588\u2557 \u2588\u2588\u2551   \u2588\u2588\u2551   \u2588\u2588\u2551   \u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255D\u2588\u2588\u2588\u2588\u2588\u2557  \r\n   \u2588\u2588\u2551   \u2588\u2588\u2551\u2588\u2588\u2551\u255A\u2588\u2588\u2554\u255D\u2588\u2588\u2551\u2588\u2588\u2551\u255A\u2588\u2588\u2554\u255D\u2588\u2588\u2551  \u255A\u2588\u2588\u2554\u255D  \u255A\u2550\u2550\u2550\u2550\u2588\u2588\u2551    \u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2551\u2588\u2588\u2551  \u2588\u2588\u2551\u255A\u2588\u2588\u2557 \u2588\u2588\u2554\u255D\u2588\u2588\u2554\u2550\u2550\u255D  \u2588\u2588\u2551\u255A\u2588\u2588\u2557\u2588\u2588\u2551   \u2588\u2588\u2551   \u2588\u2588\u2551   \u2588\u2588\u2551\u2588\u2588\u2554\u2550\u2550\u2588\u2588\u2557\u2588\u2588\u2554\u2550\u2550\u255D  \r\n   \u2588\u2588\u2551   \u2588\u2588\u2551\u2588\u2588\u2551 \u255A\u2550\u255D \u2588\u2588\u2551\u2588\u2588\u2551 \u255A\u2550\u255D \u2588\u2588\u2551   \u2588\u2588\u2551   \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2551    \u2588\u2588\u2551  \u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255D \u255A\u2588\u2588\u2588\u2588\u2554\u255D \u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\u2588\u2588\u2551 \u255A\u2588\u2588\u2588\u2588\u2551   \u2588\u2588\u2551   \u255A\u2588\u2588\u2588\u2588\u2588\u2588\u2554\u255D\u2588\u2588\u2551  \u2588\u2588\u2551\u2588\u2588\u2588\u2588\u2588\u2588\u2588\u2557\r\n   \u255A\u2550\u255D   \u255A\u2550\u255D\u255A\u2550\u255D     \u255A\u2550\u255D\u255A\u2550\u255D     \u255A\u2550\u255D   \u255A\u2550\u255D   \u255A\u2550\u2550\u2550\u2550\u2550\u2550\u255D    \u255A\u2550\u255D  \u255A\u2550\u255D\u255A\u2550\u2550\u2550\u2550\u2550\u255D   \u255A\u2550\u2550\u2550\u255D  \u255A\u2550\u2550\u2550\u2550\u2550\u2550\u255D\u255A\u2550\u255D  \u255A\u2550\u2550\u2550\u255D   \u255A\u2550\u255D    \u255A\u2550\u2550\u2550\u2550\u2550\u255D \u255A\u2550\u255D  \u255A\u2550\u255D\u255A\u2550\u2550\u2550\u2550\u2550\u2550\u255D\r\n                                                                                                                                    \r\n")
-
+    
 const readline = require('readline');
+ 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-
+    
 let playerName = '';
-
-function startGame() {
-  console.log('Welkom bij Timmy\'s avontuur!');
-  rl.question('Voer je naam in: ', (name) => {
+ 
+const intro = () => {
+  console.log("Welkom bij het avontuur van Timmy!");
+  rl.question("Wat is jouw naam? ", (name) => {
     playerName = name;
-    console.log(`Welkom, ${playerName}! Timmy gaat op avontuur.`);
-    choosePath();
+    console.log(`Welkom, ${playerName}!`);
+    startJourney();
   });
-}
-
-function choosePath() {
-  console.log(`Je staat voor een splitsing in het pad, ${playerName}.`);
-  rl.question('A. Neem het linkerpad of B. Neem het rechterpad: ', (choice) => {
-    if (choice.toLowerCase() === 'a') {
-      crossBridge();
-    } else if (choice.toLowerCase() === 'b') {
-      swimRiver();
+};
+ 
+const startJourney = () => {
+  console.log("Timmy is op weg naar het betoverde bos.");
+  rl.question("Je komt aan bij een kruispunt. Ga je links (1) of rechts (2)? ", (choice) => {
+    if (choice === '1') {
+      choice1();
+    } else if (choice === '2') {
+      choice2();
     } else {
-      console.log('Ongeldige keuze. Probeer opnieuw.');
-      choosePath();
+      console.log("Ongeldige keuze. Probeer opnieuw.");
+      startJourney();
     }
   });
-}
-
-function crossBridge() {
-  console.log('Je komt een brug tegen.');
-  rl.question('A. Steek de brug over of B. Zwem door de rivier: ', (choice) => {
-    if (choice.toLowerCase() === 'a') {
-      meetWizard();
-    } else if (choice.toLowerCase() === 'b') {
-      console.log('Je verdrinkt in de rivier. Einde 2: Game over.');
-      rl.close();
+};
+ 
+const continueJourney = () => {
+  console.log("Na je tijd in het dorp, vervolg je je reis en bereik je een mysterieus meer.");
+  rl.question("Ga je het meer verkennen (1) of besluit je verder te reizen (2)? ", (choice) => {
+    if (choice === '1') {
+      choice6();
+    } else if (choice === '2') {
+      choice7();
     } else {
-      console.log('Ongeldige keuze. Probeer opnieuw.');
-      crossBridge();
+      console.log("Ongeldige keuze. Probeer opnieuw.");
+      continueJourney();
     }
   });
-}
+};
 
-function swimRiver() {
-  console.log('Je zwemt door de rivier en komt een tovenaar tegen.');
-  rl.question('A. Vraag om hulp of B. Ga verder zonder hulp: ', (choice) => {
-    if (choice.toLowerCase() === 'a') {
-      askWizardForHelp();
-    } else if (choice.toLowerCase() === 'b') {
-      goBack();
+
+const choice1 = () => {
+  console.log("Timmy heeft de linkse weg gekozen en komt bij een betoverend meer.");
+  rl.question("Ga je zwemmen in het meer (1) of loop je verder (2)? ", (choice) => {
+    if (choice === '1') {
+      console.log(`Timmy is verdronken in het meer, GAME OVER ${playerName}.`);
+      console.log("\r\n   ____    _    __  __ _____    _____     _______ ____  \r\n  \/ ___|  \/ \\  |  \\\/  | ____|  \/ _ \\ \\   \/ \/ ____|  _ \\ \r\n | |  _  \/ _ \\ | |\\\/| |  _|   | | | \\ \\ \/ \/|  _| | |_) |\r\n | |_| |\/ ___ \\| |  | | |___  | |_| |\\ V \/ | |___|  _ < \r\n  \\____\/_\/   \\_\\_|  |_|_____|  \\___\/  \\_\/  |_____|_| \\_\\\r\n                                                        \r\n")
+      endGame();
+    } else if (choice === '2') {
+      continueJourney();
     } else {
-      console.log('Ongeldige keuze. Probeer opnieuw.');
-      swimRiver();
+      console.log("Ongeldige keuze. Probeer opnieuw.");
+      choice1();
     }
   });
-}
-
-function meetWizard() {
-  console.log('Je komt een tovenaar tegen.');
-  rl.question('A. Vraag om hulp of B. Ga verder zonder hulp: ', (choice) => {
-    if (choice.toLowerCase() === 'a') {
-      askWizardForHelp();
-    } else if (choice.toLowerCase() === 'b') {
-      goBack();
+};
+ 
+const choice2 = () => {
+  console.log("\r\n.. ........... .............  ........... . ..... ........ .......\r\n ......  ....................%.... .... ..... .........%............\r\n .@@@ ........ @@.... @@@@  . ............................  *  .....\r\n ....@@ ..... @ .... @ .............   ....... .....; .... *** .....\r\n .....\\@\\....@ .... @ ............................. #  .. *****  ...\r\n  @@@.. @@@@@  @@@@@@___.. ....... ...%..... ...  {###}  *******\r\n ....@-@..@ ..@......@@@\\...... %...... ....... <## ####>********\r\n   @@@@\\...@ @ ........\\@@@@ ..... ...... ....... {###}***********\r\n ....%..@  @@ \/@@@@@ . ....... ...............<###########> *******\r\n ...... .@-@@@@ ...V......     .... %.......... {#######}******* ***\r\n ...... .  @@ .. ..v.. .. . { } ............<###############>*******\r\n ......... @@ .... ........ {^^,     .......   {## ######}***** ****\r\n ..%..... @@ .. .%.... . .. (   `-;   ... <###################> ****\r\n . .... . @@ . .... .. _  .. `;;~~ ......... {#############}********\r\n .... ... @@ ... ..   \/(______); .. ....<################  #####>***\r\n . .... ..@@@ ...... (         (  .........{##################}*****\r\n ......... @@@  ....  |:------( )  .. <##########################>**\r\n  @@@@ ....@@@  ... _\/\/ ...... \\\\ ...... {###   ##############}*****\r\n @@@@@@@  @@@@@ .. \/ \/@@@@@@@@@ vv  <##############################>\r\n @@@@@@@ @@@@@@@ @@@@@@@@@@@@@@@@@@@ ..... @@@@@@  @@@@@@@  @@@@\r\n @@@@@@###@@@@@### @@@@@@@@@@@@@@@@@@ @@@@@@@@@@@@@@@@@@@@@@@@@@@@@\r\n @@@@@@@@###@##@@ @@@@@@@@@@@@@@@@@@@@@ @@@@@   @@@@@@@@@@@@@@@@@@@\r\n @@@@@@@@@@@### @@@@@@@@@@@@@@@@@@@@@@@@@@ @@@@@@@@@@@@@@@@@@@@@@@@\r\n -@@@@@@@@@#####@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")   
+  console.log("Timmy heeft de rechtse weg gekozen en komt bij een dichtbegroeid bos.");
+  rl.question("Ga je het bos in (1) of keer je om (2)? ", (choice) => {
+    if (choice === '1') {
+      choice3();
+    } else if (choice === '2') {
+      continueJourney();
     } else {
-      console.log('Ongeldige keuze. Probeer opnieuw.');
-      meetWizard();
+      console.log("Ongeldige keuze. Probeer opnieuw.");
+      choice2();
     }
   });
-}
+};
 
-function askWizardForHelp() {
-  console.log('De tovenaar biedt je hulp aan, maar hij wil een sleutel in ruil.');
-  rl.question('A. Vraag de tovenaar om de sleutel of B. Ga terug: ', (choice) => {
-    if (choice.toLowerCase() === 'a') {
-      console.log(`Einde 3: Je hebt de sleutel en gaat verder, ${playerName}!`);
-      rl.close();
-    } else if (choice.toLowerCase() === 'b') {
-      goBack();
-    } else {
-      console.log('Ongeldige keuze. Probeer opnieuw.');
-      askWizardForHelp();
-    }
-  });
-}
 
-function goBack() {
-  console.log(`Je gaat terug, ${playerName}. Einde 4: Je avontuur eindigt.`);
-  rl.close();
-}
-
-startGame();
+const choice3 = () => {
+    console.log("Timmy vervolgt zijn reis door het dichte bos.");
+    rl.question("Je komt bij een betoverde waterval. Ga je bij de waterval blijven (1) of doorreizen (2)? ", (choice) => {
+      if (choice === '1') {
+        console.log(`Je geniet van de betoverende waterval, ${playerName}.`);
+        endGame();
+      } else if (choice === '2') {
+        continueJourney();
+      } else {
+        console.log("Ongeldige keuze. Probeer opnieuw.");
+        choice3();
+      }
+    });
+  };
+  
+  const choice4 = () => {
+    console.log("Je betreedt het diepe bos en ontdekt een oude tempel.");
+    rl.question("Ga je de tempel betreden (1) of reis je verder (2)? ", (choice) => {
+      if (choice === '1') {
+        choice10();
+      } else if (choice === '2') {
+        continueJourney();
+      } else {
+        console.log("Ongeldige keuze. Probeer opnieuw.");
+        choice4();
+      }
+    });
+  };
+  
+  const choice5 = () => {
+    console.log("Je loopt verder en bereikt uiteindelijk het hart van het betoverde bos.");
+    console.log(`Je hebt het avontuur voltooid, ${playerName}.`);
+    endGame();
+  };
+  
+  const choice6 = () => {
+    console.log("\r\n `,.      .   .        *   .    .      .  _    ..          .\r\n     \\,~-.         *           .    .       ))       *    .\r\n          \\ *          .   .   |    *  . .  ~    .      .  .  ,\r\n ,           `-.  .            :               *           ,-\r\n  -             `-.        *._\/_\\_.       .       .   ,-\'\r\n  -                 `-_.,     |n|     .      .       ;\r\n    -                    \\ ._\/_,_\\_.  .          . ,\'         ,\r\n     -                    `-.|.n.|      .   ,-.__,\'         -\r\n      -                   ._\/_,_,_\\_.    ,-\'              -\r\n      -                     |..n..|-`\'-\'                -\r\n       -                 ._\/_,_,_,_\\_.                 -\r\n         -               ,-|...n...|                  -\r\n           -         ,-\'._\/_,_,_,_,_\\_.              -\r\n             -  ,-=-\'     |....n....|              -\r\n              -;       ._\/_,_,_,_,_,_\\_.         -\r\n             ,-          |.....n.....|          -\r\n           ,;         ._\/_,_,_,_,_,_,_\\_.         -\r\n  `,  \'.  `.  \".  `,  \'.| n   ,-.   n |  \",  `.  `,  \'.  `,  \',\r\n,.:;..;;..;;.,:;,.;:,o__|__o !.|.! o__|__o;,.:;.,;;,,:;,.:;,;;:\r\n ][  ][  ][  ][  ][  |_i_i_H_|_|_|_H_i_i_|  ][  ][  ][  ][  ][\r\n                     |     \/\/=====\\\\     |\r\n                     |____\/\/=======\\\\____|\r\n                              \/\/=========\\\\")
+    console.log("Je betreedt de oude tempel en ontdekt een magische kristal.");
+    rl.question("Wil je de kristal meenemen (1) of de tempel verlaten (2)? ", (choice) => {
+      if (choice === '1') {
+        console.log(`Je hebt de kristal meegenomen naar huis en verkoopt voor geld, ${playerName}.`);
+        endGame();
+      } else if (choice === '2') {
+        continueJourney();
+      } else {
+        console.log("Ongeldige keuze. Probeer opnieuw.");
+        choice6();
+      }
+    });
+  };
+  
+  const choice7 = () => {
+    console.log("\r\n         wWWWw               wWWWw\r\n   vVVVv (___) wWWWw         (___)  vVVVv\r\n   (___)  ~Y~  (___)  vVVVv   ~Y~   (___)\r\n    ~Y~   \\|    ~Y~   (___)    |\/    ~Y~\r\n    \\|   \\ |\/   \\| \/  \\~Y~\/   \\|    \\ |\/\r\n   \\\\|\/\/ \\\\|\/\/ \\\\|\/\/\/ \\\\|\/\/  \\\\|\/\/ \\\\\\|\/\/\/\r\n  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+    console.log("Je besluit verder te reizen en bereikt een betoverde bloementuin.");
+    rl.question("Blijf je in de bloementuin (1) of ga je verder op zoek naar nieuwe avonturen (2)? ", (choice) => {
+      if (choice === '1') {
+        console.log(`Je geniet van de betoverende bloemen, ${playerName}.`);
+        endGame();
+      } else if (choice === '2') {
+        choice8();
+      } else {
+        console.log("Ongeldige keuze. Probeer opnieuw.");
+        choice7();
+      }
+    });
+  };
+  
+  const choice8 = () => {
+    console.log("Je reist verder en komt bij een betoverd meer in het midden van het bos.");
+    rl.question("Ga je het meer verkennen (1) of reis je verder langs het meer (2)? ", (choice) => {
+      if (choice === '1') {
+        console.log(`Je ontdekt de geheimen van het betoverde meer, ${playerName}.`);
+        endGame();
+      } else if (choice === '2') {
+        choice9();
+      } else {
+        console.log("Ongeldige keuze. Probeer opnieuw.");
+        choice8();
+      }
+    });
+  };
+  
+  const choice9 = () => {
+    console.log("Je reist verder langs het betoverde meer en komt uiteindelijk aan bij een sprookjesachtig kasteel.");
+    console.log(`Je hebt een geweldige reis gemaakt, ${playerName}, en vele avonturen beleefd.`);
+    endGame();
+  };
+  
+  const choice10 = () => { 
+   console.log("Je gaat dieper de tempel in en ontdekt een verborgen schatkamer.");
+    rl.question("Wil je de schat meenemen (1) of de tempel verlaten (2)? ", (choice) => {
+      if (choice === '1') {
+        console.log(`Je hebt de schat meegenomen, ${playerName}.`);
+        endGame();
+      } else if (choice === '2') {
+        continueJourney();
+      } else {
+        console.log("Ongeldige keuze. Probeer opnieuw.");
+        choice10();
+      }
+    });
+  };
+  
+  const endGame = () => {
+    rl.close();
+  };
+  
+  intro();
+  
